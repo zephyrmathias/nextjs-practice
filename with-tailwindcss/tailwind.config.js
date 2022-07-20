ceca6ec9e1384e6a9e4049328578860c147d6a87
Ae <wuttichai.junsoontraporn@gmail.com>
2022-07-20 20:49:40 +0700

    add example with tailwindcss

---
+++ with-tailwindcss/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          400: '#FF5733',
+          DEFAULT: '#FF0000',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
