df5b675a2f0cf97b6660612ab84b637fc69c03f0
Ae <wuttichai.junsoontraporn@gmail.com>
2022-07-22 04:53:02 +0700

    adjust getStaticPaths with fallback mode

--- csr-ssr-ssg/pages/user/[id].tsx
+++ csr-ssr-ssg/pages/user/[id].tsx
@@ -1,20 +1,29 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import type { User } from '../../types/types'
 
 type Props = {
   user: User
 }
 
-const UserPage: NextPage<Props> = ({ user }: Props) => (
-  <div>
-    <div>{user.id}</div>
-    <div>{user.username}</div>
-    <div>{user.email}</div>
-  </div>
-)
+const UserPage: NextPage<Props> = ({ user }: Props) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>loading....</div>
+  }
+
+  return (
+    <div>
+      <div>{user?.id}</div>
+      <div>{user?.username}</div>
+      <div>{user?.email}</div>
+    </div>
+  )
+}
 
 export async function getStaticPaths() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users?_limit=2')
   const users = await res.json()
 
   const paths = users.map((user: User) => {
@@ -23,7 +32,14 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
+    // fallback = false -> any paths not returned by getStaticPaths() = 404 page
+    /** fallback = true 
+        -> it will try to generate a page 
+        (paths that have not bbeen generated at build time will not result 404 page)
+        it will serve fallback version of the page on the first request
+        crawler will take fallback=blocking instead (wait until page finished)
+    **/
   }
 }
 
